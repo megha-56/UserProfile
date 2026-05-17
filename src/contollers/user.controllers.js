0cbea388fcc93a9ea04fbe2e6e7806bde80ba1d5
megha-56 <meghapanthi435@gmail.com>
2026-05-17 17:45:16 +0530

    add registration workflow with controller integration

---
+++ src/contollers/user.controllers.js
@@ -0,0 +1,45 @@
+import User from "../models/user.models.js";
+import bcrypt from "bcrypt";
+
+
+// *Same for every controller*
+// export default registerUser=async ()=>{
+//     try{
+//         *all conditions*
+//         res.status(200 or 201).json({message:"Successfull"})      *200 for requestsucceded,info fetched,no new resource created)  201(if new user is created)*
+//     }catch(error){
+//         console.error("error registering User",error);
+//         res.status(500).json({message:"server error"});
+//     }
+// }
+
+export default registerUser = async ()=>{
+    try{
+        const{name,username,email,phoneNo,password}=req.body;
+
+        if(!name||!username||!email||!phoneNo||!password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        
+        const existingUser=await User.findOne({$or:[{email}, {username}, {phoneNo}]});
+        if(existingUser){
+            res.status(400).json({message:"User with this username,enail or phoneNo already exists"});
+        }
+        
+        const hashedPassword=await bcrypt.hash(password,10);
+        const newUser=new User({
+            name,
+            username,
+            email,
+            phoneNo,
+            password:hashedPassword
+        })
+        await newUser.save();
+
+
+        res.status(201).json({message:"user registered succeccfully!"});
+    }catch(error){
+        console.error("error registering user", error);
+        res.status(500).json({message:"Server Error"});
+    }
+}
