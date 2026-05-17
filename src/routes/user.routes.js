633f7dbd3fc44b7f64989f9c4a9fa421090a8b26
megha-56 <meghapanthi435@gmail.com>
2026-05-17 19:47:29 +0530

    made route for login user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import {registerUser} from "../controllers/user.controllers.js"
+import {registerUser,loginUsername} from "../controllers/user.controllers.js"
 
 const router=Router();
 
 router.post("/register",registerUser);
+router.post("/login",loginUsername);
 
 export default router;
