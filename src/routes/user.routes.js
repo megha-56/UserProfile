c1796dd47119c000f1d2b7274514b5db418ceaae
megha-56 <meghapanthi435@gmail.com>
2026-05-18 23:54:05 +0530

    made route for changePasword controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {registerUser,loginUsername,getUserProfile,editProfile} from "../contollers/user.controllers.js"
+import {registerUser,loginUsername,getUserProfile,editProfile,changePassword} from "../contollers/user.controllers.js"
 
 const router=Router();
 
 router.post("/register",registerUser);
 router.post("/login",loginUsername);
 router.post("/profile",getUserProfile);
-router.post('editProfile',editProfile)
+router.post('editProfile',editProfile);
+router.post('/changePassword',changePassword);
 
 export default router;
