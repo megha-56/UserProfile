88ea67d3cde14bc16085bc01bd5c2ffe2d2b6874
megha-56 <meghapanthi435@gmail.com>
2026-05-28 00:01:37 +0530

    changed port

--- server.js
+++ server.js
@@ -7,7 +7,13 @@ import userRoutes from './src/routes/user.routes.js'
 
 
 const app=express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+    }
+));
 dotenv.config();
 
 
